8717bdbc529c7c1d2b7885bc0f2c3601e515018b
Bimal Itani <bimalitani@Bimals-MacBook-Pro-6.local>
2025-09-23 14:23:37 -0400

    Update website: reorganized files and updated HTML/CSS/JS

---
+++ JS/btn.js
@@ -0,0 +1,23 @@
+// btn.js
+
+// Adding event listeners to each button to redirect to different URLs
+document.getElementById('btn1').addEventListener('click', function() {
+    window.location.href = 'https://github.com/bimalitani100/Personal-Website';  
+  });
+  
+  document.getElementById('btn2').addEventListener('click', function() {
+    window.location.href = 'https://github.com/bimalitani100/JPMC';  
+  });
+  
+  document.getElementById('btn3').addEventListener('click', function() {
+    window.location.href = 'https://github.com/bimalitani100/Banking-Application';  
+  });
+  
+  document.getElementById('btn4').addEventListener('click', function() {
+    window.location.href = 'https://github.com/bimalitani100/Summer-Research-2024'; 
+  });
+
+  document.getElementById('gitbtn').addEventListener('click', function() {
+    window.location.href = 'https://github.com/bimalitani100?tab=repositories';  
+  });
+  
